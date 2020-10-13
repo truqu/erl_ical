4d8264a241cd5abe764d4829ee1f23333cdbaee5
ilias <mail@ilias.xyz>
2020-10-13 11:25:30 +0200

    Add alarms

--- src/erl_ical.erl
+++ src/erl_ical.erl
@@ -18,38 +18,47 @@
          , repeat = null :: null | repeat()
          , url = null :: null | binary()
          , is_free = false :: boolean()
+         , alarms = [] :: [alarm()]
          }
        ).
+-record(alarm, {action :: binary(), description :: iodata(), trigger :: binary()}).
 
--type component() :: event().
--type repeat() :: yearly.
+-type component() :: alarm() | event().
+-type repeat() :: yearly | {monthly, pos_integer()}.
 
+-opaque alarm() :: #alarm{}.
 -opaque event() :: #event{}.
 -opaque calendar() :: #calendar{}.
 
--export_type([calendar/0, component/0, event/0, repeat/0]).
+-export_type([calendar/0, component/0, event/0, alarm/0, repeat/0]).
 
-%% Building
--export([ calendar/2
-        , with_name/2
-        , with_refresh_interval/2
-        , with_color/2
-        , event/2
+%% Building: Calendar
+-export([calendar/2, with_name/2, with_refresh_interval/2, with_color/2]).
+
+%% Building: Events
+-export([ event/2
         , with_summary/2
         , with_description/2
         , with_repeat/2
         , with_url/2
+        , with_alarm/2
         , all_day/1
         , free/1
         ]).
 
+%% Building: Alarms
+-export([display_alarm/2]).
+
 %% Encoding
 -export([encode/1]).
+
+%% Parsing (TENTATIVE)
+-export([parse/1]).
 
 -define(CRLF, "\r\n").
 
 %%==============================================================================================
-%% Building
+%% Building: Calendar
 %%==============================================================================================
 
 -spec calendar(ProdId :: binary(), Components :: [component(), ...]) -> calendar().
@@ -63,6 +72,10 @@ with_refresh_interval(Interval, Calendar) -> Calendar#calendar{refresh_interval 
 
 -spec with_color(binary(), calendar()) -> calendar().
 with_color(Color, Calendar) -> Calendar#calendar{color = Color}.
+
+%%==============================================================================================
+%% Building: Events
+%%==============================================================================================
 
 -spec event(TimeStamp :: calendar:date() | calendar:datetime1970(), UId :: binary()) -> event().
 event(TimeStamp, UId) -> #event{timestamp = TimeStamp, start = TimeStamp, uid = UId}.
@@ -79,6 +92,9 @@ with_repeat(Repeat, Event) -> Event#event{repeat = Repeat}.
 -spec with_url(binary(), event()) -> event().
 with_url(Url, Event) -> Event#event{url = Url}.
 
+-spec with_alarm(alarm(), event()) -> event().
+with_alarm(Alarm, Event) -> Event#event{alarms = [Alarm | Event#event.alarms]}.
+
 -spec all_day(event()) -> event().
 all_day(Event = #event{start = {_, _, _} = Start}) -> Event#event{'end' = add_days(Start, 1)};
 all_day(Event = #event{start = {Date, _}}) ->
@@ -90,6 +106,48 @@ add_days(Date, Days) ->
 
 -spec free(event()) -> event().
 free(Event) -> Event#event{is_free = true}.
+
+%%==============================================================================================
+%% Building: Alarm
+%%==============================================================================================
+
+-spec display_alarm(iodata(), binary()) -> alarm().
+display_alarm(Description, Trigger) ->
+  #alarm{action = <<"DISPLAY">>, description = Description, trigger = Trigger}.
+
+%%==============================================================================================
+%% Parse
+%%==============================================================================================
+
+-spec parse(iodata()) -> tql:either([KVPairs], unparseable) when
+    KVPairs :: {binary(), binary() | [KVPairs]}.
+parse(Data) ->
+  {ok, RE} = re:compile("([^:]+):(.*?)\r\n(?!\s)", [dotall]),
+  case re:run(Data, RE, [global, {capture, all, binary}, {newline, crlf}]) of
+    {match, Lines} ->
+      case
+        lists:foldr( fun ([_, Key, Value], {Acc, Stack}) ->
+                           if
+                             Key == <<"END">> -> {[], [Acc | Stack]};
+                             Key == <<"BEGIN">> ->
+                               [H | T] = Stack,
+                               {[{Value, Acc} | H], T};
+                             true ->
+                               { [{Key, binary:replace(Value, <<"\r\n ">>, <<>>, [global])}
+                                  | Acc]
+                               , Stack
+                               }
+                           end
+                     end
+                   , {[], []}
+                   , Lines
+                   )
+      of
+        {Res, []} -> {ok, Res};
+        _ -> {error, unparseable}
+      end;
+    _ -> {error, unparseable}
+  end.
 
 %%==============================================================================================
 %% Encoding
@@ -135,15 +193,23 @@ encode_component(Event = #event{}) ->
   [ kvPair("BEGIN", "VEVENT")
   , kvPair("UID", Event#event.uid)
   , kvPair("DTSTAMP", encode_timestamp(Event#event.timestamp))
-  , kvPair("DTSTART", encode_timestamp(Event#event.start))
-  , kvPair("DTEND", encode_timestamp(Event#event.'end'))
+  , kvPair("DTSTART", encode_time_or_date(Event#event.start))
+  , kvPair("DTEND", encode_time_or_date(Event#event.'end'))
   , kvPair("RRULE", encode_repeat(Event#event.repeat, Event#event.start))
   , kvPair("SUMMARY", Event#event.summary)
   , kvPair("DESCRIPTION", Event#event.description)
   , kvPair("URL;VALUE=URI", Event#event.url)
   , kvPair("X-MICROSOFT-CDO-BUSYSTATUS", encode_free(Event#event.is_free))
   , kvPair("TRANSP", encode_transparency(Event#event.is_free))
+  , lists:map(fun encode_component/1, Event#event.alarms)
   , kvPair("END", "VEVENT")
+  ];
+encode_component(Alarm = #alarm{}) ->
+  [ kvPair("BEGIN", "VALARM")
+  , kvPair("ACTION", Alarm#alarm.action)
+  , kvPair("TRIGGER", Alarm#alarm.trigger)
+  , kvPair("DESCRIPTION", Alarm#alarm.description)
+  , kvPair("END", "VALARM")
   ].
 
 -spec encode_repeat(null | repeat(), TimeStamp) -> null | iodata() when
@@ -151,14 +217,27 @@ encode_component(Event = #event{}) ->
 encode_repeat(null, _) -> null;
 encode_repeat(yearly, {_, M, D}) ->
   io_lib:format("FREQ=YEARLY;INTERVAL=1;BYMONTH=~2.10.0B;BYMONTHDAY=~2.10.0B", [M, D]);
+encode_repeat({monthly, N}, {_, _, D}) ->
+  io_lib:format("FREQ=MONTHLY;INTERVAL=~.10B;BYMONTHDAY=~2.10.0B", [N, D]);
 encode_repeat(Repeat, {Date, _}) -> encode_repeat(Repeat, Date).
+
+-spec encode_time_or_date(null) -> null;
+                         (calendar:date() | calendar:datetime1970()) -> iodata().
+encode_time_or_date(null) -> null;
+encode_time_or_date({Y, M, D}) -> io_lib:format("~4.10.0B~2.10.0B~2.10.0B", [Y, M, D]);
+encode_time_or_date({{Y, M, D}, {H, I, S}}) ->
+  io_lib:format( "~4.10.0B~2.10.0B~2.10.0BT~2.10.0B~2.10.0B~2.10.0BZ"
+               , [Y, M, D, H, I, round(S)]
+               ).
 
 -spec encode_timestamp(null) -> null;
                       (calendar:date() | calendar:datetime1970()) -> iodata().
 encode_timestamp(null) -> null;
-encode_timestamp({Y, M, D}) -> io_lib:format("~4.10.0B~2.10.0B~2.10.0B", [Y, M, D]);
+encode_timestamp({_, _, _} = Date) -> encode_timestamp({Date, {0, 0, 0}});
 encode_timestamp({{Y, M, D}, {H, I, S}}) ->
-  io_lib:format("~4.10.0B~2.10.0B~2.10.0BT~2.10.0B~2.10.0B~2.10.0BZ", [Y, M, D, H, I, S]).
+  io_lib:format( "~4.10.0B~2.10.0B~2.10.0BT~2.10.0B~2.10.0B~2.10.0BZ"
+               , [Y, M, D, H, I, round(S)]
+               ).
 
 -spec encode_free(boolean()) -> null | iodata().
 encode_free(false) -> null;
--- test/erl_ical_SUITE.erl
+++ test/erl_ical_SUITE.erl
@@ -16,6 +16,7 @@
         , with_repeat/1
         , all_day/1
         , free/1
+        , with_alarm/1
         ]).
 
 -spec all() -> [atom()].
@@ -30,6 +31,7 @@ all() ->
   , with_repeat
   , all_day
   , free
+  , with_alarm
   ].
 
 %%==============================================================================================
@@ -43,7 +45,7 @@ basic(_) ->
                    "PRODID:PRODID\r\n"
                    "BEGIN:VEVENT\r\n"
                    "UID:born\r\n"
-                   "DTSTAMP:19900507\r\n"
+                   "DTSTAMP:19900507T000000Z\r\n"
                    "DTSTART:19900507\r\n"
                    "TRANSP:OPAQUE\r\n"
                    "END:VEVENT\r\n"
@@ -55,74 +57,36 @@ basic(_) ->
 -spec with_name(any()) -> any().
 with_name(_) ->
   Calendar = erl_ical:with_name("calendar has a name", calendar(event())),
-  ?assertEqual( << "BEGIN:VCALENDAR\r\n"
-                   "VERSION:2.0\r\n"
-                   "PRODID:PRODID\r\n"
-                   "NAME:calendar has a name\r\n"
-                   "X-WR-CALNAME:calendar has a name\r\n"
-                   "BEGIN:VEVENT\r\n"
-                   "UID:born\r\n"
-                   "DTSTAMP:19900507\r\n"
-                   "DTSTART:19900507\r\n"
-                   "TRANSP:OPAQUE\r\n"
-                   "END:VEVENT\r\n"
-                   "END:VCALENDAR\r\n"
-                >>
-              , encoded(Calendar)
-              ).
+  Data = parsed(Calendar),
+  ?assertEqual(<<"calendar has a name">>, proplists:get_value(<<"NAME">>, Data)),
+  ?assertEqual(<<"calendar has a name">>, proplists:get_value(<<"X-WR-CALNAME">>, Data)).
 
 -spec with_refresh_interval(any()) -> any().
 with_refresh_interval(_) ->
   Calendar = erl_ical:with_refresh_interval("P1D", calendar(event())),
-  ?assertEqual( << "BEGIN:VCALENDAR\r\n"
-                   "VERSION:2.0\r\n"
-                   "PRODID:PRODID\r\n"
-                   "REFRESH-INTERVAL;VALUE=DURATION:P1D\r\n"
-                   "X-PUBLISHED-TTL:P1D\r\n"
-                   "BEGIN:VEVENT\r\n"
-                   "UID:born\r\n"
-                   "DTSTAMP:19900507\r\n"
-                   "DTSTART:19900507\r\n"
-                   "TRANSP:OPAQUE\r\n"
-                   "END:VEVENT\r\n"
-                   "END:VCALENDAR\r\n"
-                >>
-              , encoded(Calendar)
-              ).
+  Data = parsed(Calendar),
+  ?assertEqual(<<"P1D">>, proplists:get_value(<<"REFRESH-INTERVAL;VALUE=DURATION">>, Data)),
+  ?assertEqual(<<"P1D">>, proplists:get_value(<<"X-PUBLISHED-TTL">>, Data)).
 
 -spec with_color(any()) -> any().
 with_color(_) ->
   Calendar = erl_ical:with_color("yellow", calendar(event())),
-  ?assertEqual( << "BEGIN:VCALENDAR\r\n"
-                   "VERSION:2.0\r\n"
-                   "PRODID:PRODID\r\n"
-                   "COLOR:yellow\r\n"
-                   "BEGIN:VEVENT\r\n"
-                   "UID:born\r\n"
-                   "DTSTAMP:19900507\r\n"
-                   "DTSTART:19900507\r\n"
-                   "TRANSP:OPAQUE\r\n"
-                   "END:VEVENT\r\n"
-                   "END:VCALENDAR\r\n"
-                >>
-              , encoded(Calendar)
-              ).
+  Data = parsed(Calendar),
+  ?assertEqual(<<"yellow">>, proplists:get_value(<<"COLOR">>, Data)).
 
 -spec with_summary(any()) -> any().
 with_summary(_) ->
-  Event =
-    erl_ical:with_summary( << "this is my summary and it is pretty long, look at this summary!"
-                              " Very long, but very informative, too. Let's see if it properly"
-                              " splits!"
-                           >>
-                         , event()
-                         ),
+  Summary = << "this is my summary and it is pretty long, look at this summary! Very long, but"
+               " very informative, too. Let's see if it properly splits!"
+            >>,
+  Event = erl_ical:with_summary(Summary, event()),
+  Encoded = encoded(calendar(Event)),
   ?assertEqual( << "BEGIN:VCALENDAR\r\n"
                    "VERSION:2.0\r\n"
                    "PRODID:PRODID\r\n"
                    "BEGIN:VEVENT\r\n"
                    "UID:born\r\n"
-                   "DTSTAMP:19900507\r\n"
+                   "DTSTAMP:19900507T000000Z\r\n"
                    "DTSTART:19900507\r\n"
                    "SUMMARY:this is my summary and it is pretty long, look at this summary! Ver\r\n"
                    " y long, but very informative, too. Let's see if it properly splits!\r\n"
@@ -130,98 +94,67 @@ with_summary(_) ->
                    "END:VEVENT\r\n"
                    "END:VCALENDAR\r\n"
                 >>
-              , encoded(calendar(Event))
-              ).
+              , Encoded
+              ),
+  {ok, [{<<"VCALENDAR">>, Data}]} = erl_ical:parse(Encoded),
+  EventData = proplists:get_value(<<"VEVENT">>, Data),
+  ?assertEqual(Summary, proplists:get_value(<<"SUMMARY">>, EventData)).
 
 -spec with_description(any()) -> any().
 with_description(_) ->
   Event = erl_ical:with_description(<<"I have described.">>, event()),
-  ?assertEqual( << "BEGIN:VCALENDAR\r\n"
-                   "VERSION:2.0\r\n"
-                   "PRODID:PRODID\r\n"
-                   "BEGIN:VEVENT\r\n"
-                   "UID:born\r\n"
-                   "DTSTAMP:19900507\r\n"
-                   "DTSTART:19900507\r\n"
-                   "DESCRIPTION:I have described.\r\n"
-                   "TRANSP:OPAQUE\r\n"
-                   "END:VEVENT\r\n"
-                   "END:VCALENDAR\r\n"
-                >>
-              , encoded(calendar(Event))
-              ).
+  Data = parsed(calendar(Event)),
+  EventData = proplists:get_value(<<"VEVENT">>, Data),
+  ?assertEqual(<<"I have described.">>, proplists:get_value(<<"DESCRIPTION">>, EventData)).
 
 -spec with_url(any()) -> any().
 with_url(_) ->
   Event = erl_ical:with_url(<<"https://truqu.com/">>, event()),
-  ?assertEqual( << "BEGIN:VCALENDAR\r\n"
-                   "VERSION:2.0\r\n"
-                   "PRODID:PRODID\r\n"
-                   "BEGIN:VEVENT\r\n"
-                   "UID:born\r\n"
-                   "DTSTAMP:19900507\r\n"
-                   "DTSTART:19900507\r\n"
-                   "URL;VALUE=URI:https://truqu.com/\r\n"
-                   "TRANSP:OPAQUE\r\n"
-                   "END:VEVENT\r\n"
-                   "END:VCALENDAR\r\n"
-                >>
-              , encoded(calendar(Event))
-              ).
+  Data = parsed(calendar(Event)),
+  EventData = proplists:get_value(<<"VEVENT">>, Data),
+  ?assertEqual(<<"https://truqu.com/">>, proplists:get_value(<<"URL;VALUE=URI">>, EventData)).
 
 -spec with_repeat(any()) -> any().
 with_repeat(_) ->
-  Event = erl_ical:with_repeat(yearly, event()),
-  ?assertEqual( << "BEGIN:VCALENDAR\r\n"
-                   "VERSION:2.0\r\n"
-                   "PRODID:PRODID\r\n"
-                   "BEGIN:VEVENT\r\n"
-                   "UID:born\r\n"
-                   "DTSTAMP:19900507\r\n"
-                   "DTSTART:19900507\r\n"
-                   "RRULE:FREQ=YEARLY;INTERVAL=1;BYMONTH=05;BYMONTHDAY=07\r\n"
-                   "TRANSP:OPAQUE\r\n"
-                   "END:VEVENT\r\n"
-                   "END:VCALENDAR\r\n"
-                >>
-              , encoded(calendar(Event))
+  Event1 = erl_ical:with_repeat(yearly, event()),
+  Data1 = parsed(calendar(Event1)),
+  EventData1 = proplists:get_value(<<"VEVENT">>, Data1),
+  ?assertEqual( <<"FREQ=YEARLY;INTERVAL=1;BYMONTH=05;BYMONTHDAY=07">>
+              , proplists:get_value(<<"RRULE">>, EventData1)
+              ),
+  Event2 = erl_ical:with_repeat({monthly, 3}, event()),
+  Data2 = parsed(calendar(Event2)),
+  EventData2 = proplists:get_value(<<"VEVENT">>, Data2),
+  ?assertEqual( <<"FREQ=MONTHLY;INTERVAL=3;BYMONTHDAY=07">>
+              , proplists:get_value(<<"RRULE">>, EventData2)
               ).
 
 -spec all_day(any()) -> any().
 all_day(_) ->
   Event = erl_ical:all_day(event()),
-  ?assertEqual( << "BEGIN:VCALENDAR\r\n"
-                   "VERSION:2.0\r\n"
-                   "PRODID:PRODID\r\n"
-                   "BEGIN:VEVENT\r\n"
-                   "UID:born\r\n"
-                   "DTSTAMP:19900507\r\n"
-                   "DTSTART:19900507\r\n"
-                   "DTEND:19900508\r\n"
-                   "TRANSP:OPAQUE\r\n"
-                   "END:VEVENT\r\n"
-                   "END:VCALENDAR\r\n"
-                >>
-              , encoded(calendar(Event))
-              ).
+  Data = parsed(calendar(Event)),
+  EventData = proplists:get_value(<<"VEVENT">>, Data),
+  ?assertEqual(<<"19900507">>, proplists:get_value(<<"DTSTART">>, EventData)),
+  ?assertEqual(<<"19900508">>, proplists:get_value(<<"DTEND">>, EventData)).
 
 -spec free(any()) -> any().
 free(_) ->
   Event = erl_ical:free(event()),
-  ?assertEqual( << "BEGIN:VCALENDAR\r\n"
-                   "VERSION:2.0\r\n"
-                   "PRODID:PRODID\r\n"
-                   "BEGIN:VEVENT\r\n"
-                   "UID:born\r\n"
-                   "DTSTAMP:19900507\r\n"
-                   "DTSTART:19900507\r\n"
-                   "X-MICROSOFT-CDO-BUSYSTATUS:FREE\r\n"
-                   "TRANSP:TRANSPARENT\r\n"
-                   "END:VEVENT\r\n"
-                   "END:VCALENDAR\r\n"
-                >>
-              , encoded(calendar(Event))
-              ).
+  Data = parsed(calendar(Event)),
+  EventData = proplists:get_value(<<"VEVENT">>, Data),
+  ?assertEqual(<<"FREE">>, proplists:get_value(<<"X-MICROSOFT-CDO-BUSYSTATUS">>, EventData)),
+  ?assertEqual(<<"TRANSPARENT">>, proplists:get_value(<<"TRANSP">>, EventData)).
+
+-spec with_alarm(any()) -> any().
+with_alarm(_) ->
+  Alarm = erl_ical:display_alarm(<<"description">>, <<"trigger">>),
+  Event = erl_ical:with_alarm(Alarm, event()),
+  Data = parsed(calendar(Event)),
+  EventData = proplists:get_value(<<"VEVENT">>, Data),
+  AlarmData = proplists:get_value(<<"VALARM">>, EventData),
+  ?assertEqual(<<"DISPLAY">>, proplists:get_value(<<"ACTION">>, AlarmData)),
+  ?assertEqual(<<"trigger">>, proplists:get_value(<<"TRIGGER">>, AlarmData)),
+  ?assertEqual(<<"description">>, proplists:get_value(<<"DESCRIPTION">>, AlarmData)).
 
 %%==============================================================================================
 %% Internal functions
@@ -232,6 +165,11 @@ calendar(Event) -> erl_ical:calendar("PRODID", [Event]).
 
 -spec encoded(erl_ical:calendar()) -> binary().
 encoded(Calendar) -> list_to_binary(erl_ical:encode(Calendar)).
+
+-spec parsed(erl_ical:calendar()) -> [KVPair] when KVPair :: {binary(), binary() | [KVPair]}.
+parsed(Calendar) ->
+  {ok, [{<<"VCALENDAR">>, Data}]} = erl_ical:parse(erl_ical:encode(Calendar)),
+  Data.
 
 -spec event() -> erl_ical:event().
 event() -> erl_ical:event({1990, 5, 7}, "born").
